ebb8ff094aa4335033fb2997b9d79866ba275b5a
Avinash Patel <avinash@ceino.com>
2019-04-18 16:52:28 +0530

    Created project

---
+++ ProofOfConcepts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProofOfConcepts
+//
+//  Created by Avinash Patel on 4/18/19.
+//  Copyright © 2019 Infosys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
