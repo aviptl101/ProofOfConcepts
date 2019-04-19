1d420cfed256a47e1731adcbe3c3036a15029e87
Avinash Patel <avinash@ceino.com>
2019-04-19 13:37:24 +0530

    Added imageView, titleLabel, descriptionLabel properties in cell class

--- ProofOfConcepts/Views/PhotoCell.swift
+++ ProofOfConcepts/Views/PhotoCell.swift
@@ -9,6 +9,38 @@
 import UIKit
 
 class PhotoCell: UITableViewCell {
+    
+    private let cellImageView: UIImageView = {
+        let view = UIImageView()
+        view.layer.cornerRadius = 6
+        view.layer.masksToBounds = true
+        view.contentMode = .scaleToFill
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    public let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = Constants.Font.titleBold
+        label.textColor = .white
+        label.textAlignment = .center
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    public let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = Constants.Font.body
+        label.textColor = .white
+        label.textAlignment = .justified
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func awakeFromNib() {
         super.awakeFromNib()
